6f701742d3ec4c18666e16fb2ce0f5ee75898e00
JH. Chabran <jh@chabran.fr>
2010-05-31 15:04:52 +0200

    add missing HTTP statuses

--- lib/restclient/exceptions.rb
+++ lib/restclient/exceptions.rb
@@ -2,6 +2,8 @@ module RestClient
 
   STATUSES = {100 => 'Continue',
               101 => 'Switching Protocols',
+              102 => 'Processing',
+
               200 => 'OK',
               201 => 'Created',
               202 => 'Accepted',
@@ -9,14 +11,19 @@ module RestClient
               204 => 'No Content',
               205 => 'Reset Content',
               206 => 'Partial Content',
+              207 => 'Multi-Status',
+              
               300 => 'Multiple Choices',
               301 => 'Moved Permanently',
               302 => 'Found',
               303 => 'See Other',
               304 => 'Not Modified',
               305 => 'Use Proxy',
+              307 => 'Temporary Redirect',
+              
               400 => 'Bad Request',
               401 => 'Unauthorized',
+              402 => 'Payment Required',
               403 => 'Forbidden',
               404 => 'Resource Not Found',
               405 => 'Method Not Allowed',
@@ -32,12 +39,22 @@ module RestClient
               415 => 'Unsupported Media Type',
               416 => 'Requested Range Not Satisfiable',
               417 => 'Expectation Failed',
+              422 => 'Unprocessable Entity',
+              423 => 'Locked',
+              424 => 'Failed Dependency',
+              425 => 'No Code',
+              426 => 'Upgrade Required',
+              
               500 => 'Internal Server Error',
               501 => 'Not Implemented',
               502 => 'Bad Gateway',
               503 => 'Service Unavailable',
               504 => 'Gateway Timeout',
-              505 => 'HTTP Version Not Supported'}
+              505 => 'HTTP Version Not Supported',
+              506 => 'Variant Also Negotiates',
+              507 => 'Insufficient Storage',
+              510 => 'Not Extended'}
+              
 
   # Compatibility : make the Response act like a Net::HTTPResponse when needed
   module ResponseForException
